bee0b787290890de6fefc069a30cd36f51fed204
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-01-28 13:06:28 +0000

    Added account link to navigation

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
 import "./navigation.style.css";
 
 export default function Navigation() {
+  const { user } = useContext(UserContext);
   return (
     <nav>
       <Link to="/">Home</Link>
+      <Link to="/account">{user ? "Account" : "Register/Sign In"}</Link>
     </nav>
   );
 }
