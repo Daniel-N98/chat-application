08d483ab223eaaebdbe923e27d5b3f4528ce0a37
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-01-28 13:09:27 +0000

    Added route for account view

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Account from "../views/Account";
 import Home from "../views/Home";
 
 export default function RoutePaths() {
@@ -6,6 +7,7 @@ export default function RoutePaths() {
     <section>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
     </section>
   );
