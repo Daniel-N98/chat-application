164779d592315abd47fbd34c270454280ae1fb29
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-01-28 13:14:21 +0000

    Added conditional rendering based on user auth

--- src/views/Account.jsx
+++ src/views/Account.jsx
@@ -1,7 +1,21 @@
+import { useContext } from "react";
+import { Register, Signin } from "../components/auth";
+import { UserContext } from "../contexts/UserContext";
+
 export default function Account() {
+  const { user } = useContext(UserContext);
+
   return (
     <section id="account-view">
       <h1>Account page</h1>
+      {user ? (
+        <section id="account"></section>
+      ) : (
+        <section id="register-sign-in">
+          <Register />
+          <Signin />
+        </section>
+      )}
     </section>
   );
 }
