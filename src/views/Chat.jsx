581e9feca381aff0962e6991ed04e971044d6f25
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-01-29 21:44:54 +0000

    ChatBox implementation

--- src/views/Chat.jsx
+++ src/views/Chat.jsx
@@ -1,7 +1,10 @@
+import ChatBox from "../components/chat/ChatBox";
+
 export default function Chat() {
   return (
     <section id="chat-view">
       <h1>Chat page</h1>
+      <ChatBox />
     </section>
   );
 }
